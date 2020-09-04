e20e8652ee2c142248625165bd6bed658b02c544
jmoyson <moyson.jeremy@gmail.com>
2020-09-04 19:27:39 +0200

    Push first version of menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react"
+import { Link } from "gatsby"
+import logo from "../img/logo.svg"
 
 const Navbar = class extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       active: false,
-      navBarActiveClass: '',
+      navBarActiveClass: "",
     }
   }
 
@@ -23,10 +22,10 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active",
             })
           : this.setState({
-              navBarActiveClass: '',
+              navBarActiveClass: "",
             })
       }
     )
@@ -35,58 +34,47 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
+        className='navbar is-transparent'
+        role='navigation'
+        aria-label='main-navigation'
       >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
+        <div className='container'>
+          <div className='navbar-brand left-burger'>
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
+              data-target='navMenu'
               onClick={() => this.toggleHamburger()}
             >
-              <span />
-              <span />
-              <span />
+              <span aria-hidden='true'></span>
+              <span aria-hidden='true'></span>
+              <span aria-hidden='true'></span>
             </div>
+            <Link to='/' className='navbar-item' title='Logo'>
+              <img src={logo} alt='MerakiOse' style={{ width: "88px" }} />
+            </Link>
+            {/* Hamburger menu */}
           </div>
+
           <div
-            id="navMenu"
+            id='navMenu'
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+            <div className='navbar-start has-text-centered'>
+              <Link className='navbar-item' to='/about'>
+                À Propos
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className='navbar-item' to='/products'>
+                Boutique
               </Link>
-              <Link className="navbar-item" to="/blog">
+              <Link className='navbar-item' to='/products'>
+                Coaching
+              </Link>
+              <Link className='navbar-item' to='/blog'>
                 Blog
               </Link>
-              <Link className="navbar-item" to="/contact">
+              <Link className='navbar-item' to='/contact'>
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
